648640aaf4f1ec24177c8e8a20b02fd2869fdf74
Munkhbayar Nergui <muunuu17@gmail.com>
2017-08-04 19:37:10 +0100

    init

---
+++ server/src/config.js
@@ -0,0 +1,13 @@
+// obtain API key from your Skyscanner contact
+const APIKEY = 'ss630745725358065467897349852985';
+
+if (!APIKEY) {
+  console.error('APIKEY environment variable missing. ' +
+    'Please re-run with `APIKEY=<key> npm run server`');
+  process.exit(1);
+}
+
+module.exports = {
+  apiKey: APIKEY,
+  skyscannerApi: 'http://partners.api.skyscanner.net/'
+};
